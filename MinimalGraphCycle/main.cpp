01973b68345fb0391a104270de1fb06eb7776236
ale110i <ishhenko.as@phystech.edu>
2019-02-22 20:16:53 +0300

    Initial Commit

---
+++ MinimalGraphCycle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MinimalGraphCycle
+//
+//  Created by Alexey Ischenko on 22/02/2019.
+//  Copyright © 2019 Enthropy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
